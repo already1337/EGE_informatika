6f8a7be071975bf337b5448b6ccc267e9a7a83df
already1337 <sem-137@mail.ru>
2024-04-19 22:00:46 +0800

    .

--- ege15/ege15.5/other/ege15.5-5.py
+++ ege15/ege15.5/other/ege15.5-5.py
@@ -2,8 +2,8 @@ for a in range(1, 1000):
     f = True
     for x in range(1, 1000):
         for y in range(1, 1000):
-            if (not (2*y+3*x==135) or (y>a) or (x>a))==0:
-                f=False
+            if (not (2 * y + 3 * x == 135) or (y > a) or (x > a)) == 0:
+                f = False
     if f:
         print(a)
 
