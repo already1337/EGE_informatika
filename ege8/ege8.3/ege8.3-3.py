e148760842374658fff473955a19500a58029214
already1337 <sem-137@mail.ru>
2024-05-17 01:05:52 +0800

    .

--- ege8/ege8.3/ege8.3-3.py
+++ ege8/ege8.3/ege8.3-3.py
@@ -1,6 +1,6 @@
 from itertools import product
 alphabet = '0123'
-ap=[]
+ap = []
 for i in product(alphabet, repeat=3):    
     if (i[0] != '0') and (int(i[0]) + int(i[2]) > int(i[1])):
         ap.append(i)
